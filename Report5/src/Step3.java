b95d954fa1bc7891bbaaaa90ae1076fd6aa12154
Kenta Kinjo <e245701@ie.u-ryukyu.ac.jp>
2024-12-17 23:33:13 +0900

    Step3

---
+++ Report5/src/Step3.java
@@ -0,0 +1,7 @@
+public class Step3 {
+    public static void main(String[] args) {
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
+    }
+}
